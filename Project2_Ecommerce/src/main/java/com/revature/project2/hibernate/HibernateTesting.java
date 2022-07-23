0790584c3871ec24f21bac9b04afa0dda36df3b5
AngelaHsiao <angelaphsiao@gmail.com>
2022-07-23 16:35:59 -0700

    hibernate test

---
+++ Project2_Ecommerce/src/main/java/com/revature/project2/hibernate/HibernateTesting.java
@@ -0,0 +1,16 @@
+package com.revature.project2.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.project2.pojo.Customer;
+
+public class HibernateTesting {
+public static void main(String[] args) {
+	Session session = HibernateUtils.getSessionFactory().openSession();
+	Customer c = new Customer("testing1","testpwd","test@test.test");
+	Transaction tx = session.beginTransaction();
+	session.save(c);
+	tx.commit();
+}
+}
